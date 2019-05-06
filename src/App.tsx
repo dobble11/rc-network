62c530bfba16fa7caad2768bfbc46e81187f97ba
zhangjiawei <zhangjiawei@zhangjiaweideMacBook-Pro.local>
2019-05-06 17:13:23 +0800

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
+import { Tabs } from 'antd';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const TabPane = Tabs.TabPane;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Tab 1" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="Tab 2" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Tab 3" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+      </Tabs>
     </div>
   );
-}
+};
 
 export default App;
